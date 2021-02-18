b40d796572597eb758e245ebccc436b2f1ef88f6
NestorPons <nestorpons@gmail.com>
2021-02-18 07:52:28 +0100

    Inicio menu

--- Modules/Menu.py
+++ Modules/Menu.py
@@ -2,10 +2,30 @@ import re
 import requests
 
 class Menu:
-    def __init__(self):
-        self.res = input('Introduzca una url:')
+    def __init__(self, object):
+        pass
+    
+    def requestTarget(self, default = ''):
+        self.res = input('Introduzca un objetivo url [{}]: '.format(default))
+        if self.res == '':
+            self.res = default
+            
         self.res = self.isURL(self.res)
-        
+        return self.res
+    
+    def mainMenu(self):   
+        res = input('''
+              Seleccione una opción:
+              1 Obtener las imagenes\n
+              2 Obtener las urls\n
+              3 Obtener los emails\n
+              4 Crear informe\n
+              ''')
+        if res == 1:
+            res = input('Seleccione objetivo [Todo]')
+            if re == '': 
+                
+    
     def isURL(self, url):
         if not re.search('^(https|http)+://', url):
             try:
--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 from Modules.Browser import Browser
 from Modules.Menu import Menu
 
-menu = Menu()
+target = Browser()
+menu = Menu(target)
+res = menu.requestTarget('www.lebouquet.es')
 
-target = Browser(menu.getRes())
 # Buscamos todas las referencias
 target.searchRef()
 target.viewImages(0)
