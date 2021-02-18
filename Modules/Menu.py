c4194b7797910e24651461d3ad6a98e46db4f307
nestorpons <nestorpons@gmail.com>
2021-02-18 14:57:36 +0100

    Implementado menu de terminal

--- Modules/Menu.py
+++ Modules/Menu.py
@@ -1,41 +1,61 @@
 import re
 import requests
+import sys 
 
 class Menu:
-    def __init__(self, object):
-        pass
+    
+    strOptions = ''
+    mnuOptions = {}
+
+    def __init__(self):
+        "clear" if sys.platform.startswith("linux") else "cls"
     
     def requestTarget(self, default = ''):
-        self.res = input('Introduzca un objetivo url [{}]: '.format(default))
-        if self.res == '':
-            self.res = default
-            
-        self.res = self.isURL(self.res)
-        return self.res
-    
-    def mainMenu(self):   
-        res = input('''
-              Seleccione una opción:
-              1 Obtener las imagenes\n
-              2 Obtener las urls\n
-              3 Obtener los emails\n
-              4 Crear informe\n
-              ''')
-        if res == 1:
-            res = input('Seleccione objetivo [Todo]')
-            if re == '': 
+        try:
+            self.res = input('Introduzca un objetivo url [{}]: '.format(default))
+            if self.res == '':
+                self.res = default
                 
-    
+            self.res = self.isURL(self.res)
+            print('Objetivo:', self.res)
+            return self.res
+
+        except ValueError as v:
+            print('ERROR: url no válida!')
+            self.requestTarget(default)
+
+        except Exception as e:
+            print(e)
+
+    def start(self, default = None):  
+        print (self.strOptions, '\n')
+        try:
+            res = int(input('Seleccione una opción: '))
+        except:
+            print('\nERROR: Debe introducir un número de opción!!')
+            self.start()
+        
+        #Ejecutamos el método vinculado al índice del menú seleccionado
+        return [res,self.mnuOptions[res]()]
+        
+    def addOption(self, index, description, action):
+        self.mnuOptions[int(index)] = action
+        self.strOptions = '{}\n{} {}'.format(self.strOptions, index, description) 
+
     def isURL(self, url):
-        if not re.search('^(https|http)+://', url):
-            try:
-                u = 'https://{}'.format(url)
-                requests.get(u)                
-            except:
-                u = 'http://{}'.format(url)
-                requests.get(u)
-            return u
+        if url == '' or re.search('(.*?)\.(.*?)\.(.*?)', url):
+            if not re.search('^(https|http)+://', url):
+                try:
+                    u = 'https://{}'.format(url)
+                    requests.get(u)                
+                except:
+                    u = 'http://{}'.format(url)
+                    requests.get(u)
+                return u
+            else:
+                return url
         else:
-            return url
+            raise ValueError
+    
     def getRes(self):
         return self.res
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 
-from Modules.Browser import Browser
+from Modules.Scanner import Scanner
 from Modules.Menu import Menu
+from tabulate import tabulate
 
-target = Browser()
-menu = Menu(target)
-res = menu.requestTarget('www.lebouquet.es')
+menu = Menu()
+scann = Scanner()
 
-# Buscamos todas las referencias
-target.searchRef()
-target.viewImages(0)
+menu.addOption('1', 'Obtener los telefonos', scann.getTels)
+menu.addOption('2', 'Obtener los emails', scann.viewEmails)
+
+
+t = menu.requestTarget('www.lebouquet.es')
+scann.setTarget(t)
+# Se inicia el scaneo de todas las páginas vinculadas
+scann.scanLinks()
+
+# Se muestra el menú y esperamos la respuesta 
+response = menu.start()
+if response[0] == 1:
+    print(response[1][0])
+    print(tabulate(response[1][1].items(), showindex=True))
+
+elif response[0] == 2:
+    pass
