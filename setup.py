348f68c2d94b1bbd0910881c7326757f7e7c4c08
nestorpons <nestorpons@gmail.com>
2021-05-19 11:41:52 +0200

    gitignore añadido archivos de entorno

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='scrappy',
+      version='0.1.0',
+      description='Scrapeo de paginas web',
+      long_description=open('README.md').read(),
+      url='https://github.com/nestorPons/scrappy',
+      author='Néstor Pons',
+      author_email='nestorpons@gmail.com',
+      license='MIT',
+      packages=[],
+      install_requires=[
+          'pyexiv2', 
+          'tabulate', 
+          'requests',
+          'wget',
+          'PyExifTool',
+          'mechanize >= 0.4.4',
+          'progress == 1.5'
+      ],
+      zip_safe=False)
